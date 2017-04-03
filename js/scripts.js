82a78d204af760d352e0e694ace1a1551c0e2ff9
Anna Horodetska and David Shin <david@>
2017-04-03 15:07:39 -0700

    fix syntax and improve functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,28 @@
-function Destination(location, landmark, time, notes) {
-  this.location = location;
-  this.landmark = landmark;
-  this.time = time;
-  this.notes = notes;
+$(document).ready(function() {
+
+function Task(task) {
+  this.task = task;
 }
 
-$(document).ready(function() {
-  $("form#new-destination").submit(function(event) {
+  $("form#form").submit(function(event) {
     event.preventDefault();
+    var inputtedTask = $("input#new-task").val();
+    var newTask = new Task(inputtedTask);
 
-    var inputtedLocation = $("input#new-location").val();
-    var inputtedLandmark = $("input#new-landmark").val();
-    var inputtedTime = $("input#new-time").val();
-    var inputtedNotes = $("input#new-notes").val();
+    $("input#new-task").val("");
 
-    var newDestination= new Destination(inputtedLocation, inputtedLandmark, inputtedTime, inputtedNotes);
+    $("ul#tasks").append('<li>'+'<div class="checkbox">' + '<input type="checkbox" value=" ">' + newTask.task + '</div>'+'</li>');
 
-    $("ul#destination").append("<li><span class='destination'>" + newDestination.location + "</span></li>");
-
-    $("input#new-location").val("");
-    $("input#new-landmark").val("");
-    $("input#new-time").val("");
-    $("input#new-notes").val("");
-
-    $(".destination").click(function() {
-    $("#show-destination").show();
-    $("#show-destination h2").text(newDestination.location);
-    $(".location").text(newDestination.location);
-    $(".landmark").text(newDestination.landmark);
-    $(".time").text(newDestination.time);
-    $(".notes").text(newDestination.notes);
+    $("#btn2").show();
   });
 
-});
+  $("#btn2").click(function(){
+    var checked = [];
+           $.each($("input[name='tasksToDo']:checked"), function(){
+               checked.push($(this).val());
+               console.log($(this).val());
+           });
+  });
+
+
 });
